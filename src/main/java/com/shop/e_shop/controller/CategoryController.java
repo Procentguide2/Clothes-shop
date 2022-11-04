f60daf87b9c9f54c0f7a4a834fec70bf522b2c62
Kalarashan <kalarasankonstantin@gmail.com>
2022-11-04 18:22:49 +0200

    added CrossOrigin

--- src/main/java/com/shop/e_shop/controller/CategoryController.java
+++ src/main/java/com/shop/e_shop/controller/CategoryController.java
@@ -5,15 +5,13 @@ import com.shop.e_shop.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@CrossOrigin
 @RestController
 public class CategoryController {
 
