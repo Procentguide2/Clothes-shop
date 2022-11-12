9bf749ca2c52f3d461a50f05ea7fb770108e98f4
Kalarashan <kalarasankonstantin@gmail.com>
2022-11-12 16:35:17 +0200

    added GeneratedValue to color id

--- src/main/java/com/shop/e_shop/model/Color.java
+++ src/main/java/com/shop/e_shop/model/Color.java
@@ -1,14 +1,12 @@
 package com.shop.e_shop.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "color")
 public class Color {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
