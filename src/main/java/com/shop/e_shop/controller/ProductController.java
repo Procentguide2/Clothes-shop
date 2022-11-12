7eec0eff82a30eb60c5392569e45350f89834dc6
Kalarashan <kalarasankonstantin@gmail.com>
2022-11-12 15:33:41 +0200

    updated code

--- src/main/java/com/shop/e_shop/controller/ProductController.java
+++ src/main/java/com/shop/e_shop/controller/ProductController.java
@@ -7,7 +7,6 @@ import com.shop.e_shop.service.FavoriteService;
 import com.shop.e_shop.service.ProductService;
 import com.shop.e_shop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
