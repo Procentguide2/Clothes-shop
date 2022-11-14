52cb58fda6ce8cb6c71f2a4b57b80eeaf1a24b5b
Kalarashan <kalarasankonstantin@gmail.com>
2022-11-14 22:10:07 +0200

    minor update

--- src/main/java/com/shop/e_shop/controller/UserController.java
+++ src/main/java/com/shop/e_shop/controller/UserController.java
@@ -4,10 +4,14 @@ import com.shop.e_shop.model.EmailWrapper;
 import com.shop.e_shop.model.User;
 import com.shop.e_shop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -17,9 +21,14 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/user/register")
-    public void userRegistration(@RequestBody User user){
+    public ResponseEntity<String> userRegistration(@RequestBody User user){
+        Optional<User> foundUser = Optional.ofNullable(userService.findUserByEmail(user.getEmail()));
+        if (foundUser.isPresent()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         user.setIsAdmin("0");
         userService.saveUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/user/get/email")
